f551209af489b72174d7818a43b06b224a8b32de
HPxIO <hpx.io@keemail.me>
2022-05-08 22:00:05 +0530

    init-project

---
+++ getting-started-with-spring-batch-basics/src/main/java/com/app/hpx/gettingstartedwithspringbatchbasics/GettingStartedWithSpringBatchBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.app.hpx.gettingstartedwithspringbatchbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingStartedWithSpringBatchBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GettingStartedWithSpringBatchBasicsApplication.class, args);
+	}
+
+}
---
+++ getting-started-with-spring-batch-basics/src/test/java/com/app/hpx/gettingstartedwithspringbatchbasics/GettingStartedWithSpringBatchBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.hpx.gettingstartedwithspringbatchbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingStartedWithSpringBatchBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
